d1d56351bd9dd1a9d7397847c902cfeb2fb51380
Oleksii Tsvietnov <me@vorakl.name>
2017-06-05 00:18:24 +0200

    Add new functions: format, lstrip, rstrip

---
+++ examples/common-example8.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+main() {
+    source /home/vorakl/repos/my/github/lib-sh/common
+
+    readlines -r out < <(date)
+    printf "\nRaw results:\n"
+    printf "($out)\n"
+
+    rstrip $'\n' out <<< "$out"
+    printf "\nStrip new line at the end:\n"
+    printf "($out)\n"
+
+
+    printf "\nAdd extra symbols \"........\"\n"
+    date | format "(%s.......)\n"
+    date | format "%s.......\n" | rstrip "." out
+    printf "\nStrip \"........\" from the right\n"
+    printf "($out)\n"
+
+    date '+%s' | format "\nThe current time: %(%H:%M:%S)T\n"
+
+    format "%14.2f" out <<< "1.4899999"
+    printf "\nA formated digit \"1.4899999\": (${out})\n"
+
+    str="        Hello"
+    lstrip " " out <<< "${str}"
+    printf "\nThe raw string (${str})\n"
+    printf "Strip spaces from the left (${out})\n"
+}
+
+main
