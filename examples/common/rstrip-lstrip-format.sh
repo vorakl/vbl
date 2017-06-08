fd0dccd69d32e6a23f52c79f56e1404852b6cf10
Oleksii Tsvietnov <me@vorakl.name>
2017-06-08 14:55:52 +0200

    Fix format function

--- examples/common/rstrip-lstrip-format.sh
+++ examples/common/rstrip-lstrip-format.sh
@@ -13,15 +13,17 @@ main() {
 
 
     printf "\nAdd extra symbols \"........\"\n"
-    date | format "(%s.......)\n"
-    date | format "%s.......\n" | rstrip "." out
+    date | rstrip $'\n' | format "(%s.......)\n"
+    date | rstrip $'\n' | format "%s.......\n" | rstrip "." out
     printf "\nStrip \"........\" from the right\n"
     printf "($out)\n"
 
-    date '+%s' | format "\nThe current time: %(%H:%M:%S)T\n"
+    date '+%s' | rstrip $'\n' | format "\nThe current time: %(%H:%M:%S)T\n"
 
-    format "%14.2f" out <<< "1.4899999"
+    format "%14.2f" out "1.4899999"
     printf "\nA formated digit \"1.4899999\": (${out})\n"
+
+    format "\nAnother formated digit: %08.2f\n" - "-1.34567"
 
     str="        Hello"
     lstrip " " out <<< "${str}"
