13218657d1059ed6fc493b3a8d574248f676a9e0
Oleksii Tsvietnov <me@vorakl.name>
2017-06-15 20:57:24 +0200

    Add more tests

--- tests/common-test.sh
+++ tests/common-test.sh
@@ -44,31 +44,59 @@ it_checks_cmd() {
 }
 
 it_checks_run_output_stdout() {
-    output="$(run eval '{ say "hello"; err "world"; }')"
-    [[ "${output}" == "hello" ]]
+    output="$(run eval '{ say "Hello"; err "World"; }')"
+    [[ "${output}" == "Hello" ]]
 
-    output="$(run --no-out eval '{ say "hello"; err "world"; }')"
+    output="$(run --no-out eval '{ say "Hello"; err "World"; }')"
     [[ -z "${output}" ]]
 }
 
 it_checks_run_output_stderr() {
     set +x
-    output="$(run err "error" 2>&1)"
-    [[ "${output}" == "error" ]]
+    output="$(run err "Error" 2>&1)"
+    [[ "${output}" == "Error" ]]
 
-    output="$(run --no-err err "error" 2>&1)"
+    output="$(run --no-err err "Error" 2>&1)"
     [[ -z "${output}" ]]
 }
 
 it_checks_run_output_join() {
     set +x
-    output="$(run --join-outerr eval '{ SAY_FORMAT="%s" say "hello "; err "world"; }')"
-    [[ "${output}" == "hello world" ]]
+    output="$(run --join-outerr eval '{ SAY_FORMAT="%s" say "Hello "; err "World"; }')"
+    [[ "${output}" == "Hello World" ]]
 }
 
 it_checks_run_output_silent() {
     set +x
-    output="$(run --silent eval '{ SAY_FORMAT="%s" say "hello "; err "world"; }' 2>&1)"
+    output="$(run --silent eval '{ say "Hello "; err "World"; }' 2>&1)"
     [[ -z "${output}" ]]
+}
+
+it_checks_run_output_save() {
+    set +x
+    run --save-out my_out --save-err my_err eval '{ say "Hello"; err "World"; }'
+    [[ "${my_out/$'\n'/}" == "Hello" ]]
+    [[ "${my_err/$'\n'/}" == "World" ]]
+}
+
+it_checks_run_status_ignore() {
+    run --ignore false
+}
+
+it_checks_run_status_warn() {
+    set +x
+    output="$(RUN_WARN_FORMAT="%s-%d\n" run --warn --no-err cat /nonexistent 2>&1)"
+    [[ "${output}" == "cat-1" ]]
+}
+
+it_checks_run_status_ensure() {
+    set +x
+    if output="$(DIE_EXITCODE=14; RUN_ENSURE_FORMAT="%s-%d-%d\n" run --ensure --no-err cat /nonexistent 2>&1)"
+    then
+        echo "This test does not work"
+        return 1
+    else
+        [[ "${output}" == "cat-1-14" ]]
+    fi
 }
 
