984bee2898f97e923d999aa00cf17301810ed0d5
Oleksii Tsvietnov <me@vorakl.name>
2019-01-03 02:48:25 +0100

    Chang host name

--- src.docs/pelicanconf.py
+++ src.docs/pelicanconf.py
@@ -7,9 +7,9 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Oleksii Tsvietnov'
 SITENAME = u"Bash Libraries"
-SITEURL = 'https://bashlib.cf'
+SITEURL = 'http://libs.cf'
 SITEDESC = u'A collection of Bash libraries'
-SITE_VERSION = '1497518367'
+SITE_VERSION = '1497518368'
 SITE_KEYWORDS = 'bash,shell,library,bash-library,shell-library'
 ARTICLE_PATHS = ['articles'] # a place for articles under the content location
 PAGE_PATHS = ['pages']
